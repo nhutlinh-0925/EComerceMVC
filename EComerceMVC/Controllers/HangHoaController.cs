f24b7635bf0a891dfaeb4a1bf404a8f7c83c47a6
Nhut Linh <trannhutlinh0925@gmail.com>
2024-01-06 21:17:29 +0700

    Menu Hàng Hóa

---
+++ EComerceMVC/Controllers/HangHoaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EComerceMVC.Controllers
+{
+    public class HangHoaController : Controller
+    {
+        public IActionResult Index(int? Loai)
+        {
+            return View();
+        }
+    }
+}
