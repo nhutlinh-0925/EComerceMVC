b6a37fbd159db7cbbded2b80f8df3828fbd4e576
Nhut Linh <trannhutlinh0925@gmail.com>
2024-01-23 22:56:00 +0700

    Thanh toán đơn hàng

--- EComerceMVC/Controllers/CartController.cs
+++ EComerceMVC/Controllers/CartController.cs
@@ -2,16 +2,17 @@
 using EComerceMVC.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using EComerceMVC.Helpers;
+using Microsoft.AspNetCore.Authorization;
 
 namespace EComerceMVC.Controllers
 {
-	public class CartController : Controller
-	{
-		private readonly Hshop2023Context db;
-		public CartController(Hshop2023Context context)
-		{
-			db = context;
-		}
+    public class CartController : Controller
+    {
+        private readonly Hshop2023Context db;
+        public CartController(Hshop2023Context context)
+        {
+            db = context;
+        }
 
         public List<CartItem> Cart => HttpContext.Session.Get<List<CartItem>>(MySetting.CART_KEY) ?? new List<CartItem>();
 
@@ -64,5 +65,81 @@ namespace EComerceMVC.Controllers
             return RedirectToAction("Index");
         }
 
-    }
+        [Authorize]
+        [HttpGet]
+        public IActionResult Checkout()
+        {
+            if (Cart.Count == 0)
+            {
+                return Redirect("/");
+            }
+
+            return View(Cart);
+        }
+
+		[Authorize]
+		[HttpPost]
+		public IActionResult Checkout(CheckoutVM model)
+		{
+			if (ModelState.IsValid)
+			{
+				var customerId = HttpContext.User.Claims.SingleOrDefault(p => p.Type == MySetting.CLAIM_CUSTOMERID).Value;
+				var khachHang = new KhachHang();
+				if (model.GiongKhachHang)
+				{
+					khachHang = db.KhachHangs.SingleOrDefault(kh => kh.MaKh == customerId);
+				}
+
+				var hoadon = new HoaDon
+				{
+					MaKh = customerId,
+					HoTen = model.HoTen ?? khachHang.HoTen,
+					DiaChi = model.DiaChi ?? khachHang.DiaChi,
+					DienThoai = model.DienThoai ?? khachHang.DienThoai,
+					NgayDat = DateTime.Now,
+					CachThanhToan = "COD",
+					CachVanChuyen = "GRAB",
+					MaTrangThai = 0,
+					GhiChu = model.GhiChu
+				};
+
+				db.Database.BeginTransaction();
+				try
+				{
+					db.Database.CommitTransaction();
+					db.Add(hoadon);
+					db.SaveChanges();
+
+					var cthds = new List<ChiTietHd>();
+					foreach (var item in Cart)
+					{
+						cthds.Add(new ChiTietHd
+						{
+							MaHd = hoadon.MaHd,
+							SoLuong = item.SoLuong,
+							DonGia = item.DonGia,
+							MaHh = item.MaHh,
+							GiamGia = 0
+						});
+					}
+					db.AddRange(cthds);
+					db.SaveChanges();
+
+					HttpContext.Session.Set<List<CartItem>>(MySetting.CART_KEY, new List<CartItem>());
+
+					return View("Success");
+				}
+				catch
+				{
+					db.Database.RollbackTransaction();
+				}
+			}
+
+			return View(Cart);
+		}
+
+
+
+
+	}
 }
